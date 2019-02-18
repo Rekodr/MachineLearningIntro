e9c63dc8d423ccc5d60b20048bca371b95f17976
Recodeo Rekod <recodeo.rekod@adtegrity.com>
2019-02-18 14:05:15 -0500

    create data without stopwords

---
+++ removeStopWords.py
@@ -0,0 +1,35 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_PATH = os.path.join(BASE_DIR, "sample_data", "Forum")
+TRAINING_FILE = ("forumTraining.data.txt", "forumTraining-clean.data.txt")
+TEST_FILE =  ("forumTest.data.txt", "forumTest-clean.data.text")
+
+
+def clean():
+    stop_words = set(stopwords.words('english')) 
+    for origin, new in [TRAINING_FILE, TEST_FILE]:
+        origin_file = os.path.join(BASE_PATH, origin)
+        new_file = os.path.join(BASE_PATH, new)
+
+        W = ""
+        with open(origin_file, "r") as f:
+            for line in f:
+                l = ""
+                words = line.split()
+                for w in words:
+                    if not w in stop_words:
+                        l += w + " "
+                l += "\n"
+                W += l
+        f.close()
+        dst = open(new_file, "w")
+        dst.write(W)
+        dst.close()
+
+if __name__ == "__main__":
+    clean()
+
+
