c84fd2735adaf2cc37136c9dfada46bf99be92fc
Recodeo Rekod <rekodr@mail.gvsu.edu>
2019-03-16 12:48:41 -0400

    wip

--- anylize.py
+++ anylize.py
@@ -5,7 +5,7 @@ dt2 :pd.DataFrame = pd.read_csv("resultsTree.csv") # -
 dt3 :pd.DataFrame = pd.read_csv("resultsTree2.csv") # -
 dt5 :pd.DataFrame = pd.read_csv("t.csv") # +
 dt6 :pd.DataFrame = pd.read_csv("resultsTree4.csv") # nursing
-dt7 :pd.DataFrame = pd.read_csv("resultsNursing.csv")
+dt7 :pd.DataFrame = pd.read_csv("resultsNursing2.csv")
 # print(dt2.describe())
 # print(dt3.describe())
 # print(dt5.describe())
@@ -13,11 +13,11 @@ dt7 :pd.DataFrame = pd.read_csv("resultsNursing.csv")
 # print(dt6.describe())
 # print(dt7.describe())
 
-df = dt6
-print(df.describe())
-idx=df.groupby(by=['cut', "split_attr", "pruned"])['accuracy'].idxmax()
-#idx=df.groupby(by=['trees', "split_attr"])['accuracy'].idxmax()
+df = dt7
+# print(df())
+#idx=df.groupby(by=['cut', "split_attr", "pruned"])['accuracy'].idxmax()
+idx=df.groupby(by=['trees'])['accuracy'].idxmax()
 df_max=df.loc[idx,]
 print("*" * 20)
 print(df_max)
-# df_max.to_csv("rf_analysis.csv", index=False)
+df_max.to_csv("rf_analysis.csv", index=False)
